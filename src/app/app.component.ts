56f2f9ab7770d307590a0b74128ca156d2cd8b47
Senthilnathan Kirisanth <kirisanth.14@cse.mrt.ac.lk>
2017-09-26 18:12:02 +0530

    heroes list added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,29 @@ export class Hero {
     
 }
 
+const heroes: Hero[] = [
+    { id: 2, name: 'Kirisanth' },
+    { id: 3, name: 'Gowmaran' },
+    { id: 4, name: 'Gunesh' },
+    { id: 5, name: 'Brundha' },
+    { id: 6, name: 'Kajan' },
+    { id: 7, name: 'Vaiz' },
+    { id: 8, name: 'Sapthami' },
+    { id: 9, name: 'Niwash' },
+    { id: 10, name: 'Nio' }
+];
+
+
 @Component({
   selector: 'my-app',
   template: `
         <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+        <ul class="Heroes">
+          <li *ngFor="let hero of Heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+        </ul>
         <h2>{{hero.name}} details</h2>
         <div>
           <label>id: </label>{{hero.id}}
@@ -26,24 +45,15 @@ export class AppComponent  {
     hero = 'Spiderman';
     name = 'Kirisanth';
 
+     
     hero: Hero = {
         id: 1,
         name: 'Windstrom'
     };
 
-    const heroes: Hero[
-    { id: 02, name: "Kirisanth" },
-    { id: 03, name: "Gowmaran" },
-    { id: 04, name: "Gunesh" },
-    { id: 05, name: "Brundha" },
-    { id: 06, name: "Kajan" },
-    { id: 07, name: "Vaiz" },
-    { id: 08, name: "Sapthami" },
-    { id: 09, name: "Niwash" },
-    { id: 10, name: "Nio" },
-    ];
-
-    Heroes = heroes;
+    Heroes = heroes; 
+       
+    
 
 }
 
